f88c713e402e71b085c76a3523239e283154e1d3
ahmedmohsamir <ahmed218816@bue.edu.eg>
2025-07-15 14:30:21 +0300

    Display, Exit , & Clock

--- Game.py
+++ Game.py
@@ -1,1 +1,23 @@
+import pygame
+pygame.init()
 
+screen=pygame.display.set_mode((800,400))
+pygame.display.set_caption("Cat_Runner")
+clock=pygame.time.Clock()
+
+plain_surface = pygame.Surface((100,200))
+plain_surface.fill('Red')
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+
+
+
+    screen.blit(plain_surface,(0,0))
+    pygame.display.update()
+    clock.tick(60)  
